289d3d999f13e3925013955f57ed198a33a1eb46
areph <40balmung@gmail.com>
2022-03-17 22:38:25 +0900

    Change GET method to POST

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const apigwUrl = 'https://8rufvf08rh.execute-api.ap-northeast-1.amazonaws.com/dev/translate?input_text='
+const apigwUrl = 'https://8rufvf08rh.execute-api.ap-northeast-1.amazonaws.com/dev/translate'
 const submitButton = document.getElementById('submit')
 submitButton.addEventListener('click', async () => {
     const textMessage = document.getElementById('message')
     console.log("textMessage:" + textMessage.value)
 
     // AjaxでAPIGWへGET
-    const response = await fetch(apigwUrl + textMessage.value, {
-        method: 'GET',
-        mode: 'cors',
-        // body: "param=" + paramVar,
+    const response = await fetch(apigwUrl, {
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json'
-    }})
+        },
+        body: JSON.stringify({input_text: textMessage.value})
+    })
     const json = await response.json()
     console.log(json['output_text']);
     document.getElementById('translated-message').value = json['output_text']
