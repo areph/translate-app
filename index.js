1d132ce4acb652eeb13d9bdaa1b8f312f611203c
areph <40balmung@gmail.com>
2022-03-17 22:57:53 +0900

    fix

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const apigwUrl = 'https://8rufvf08rh.execute-api.ap-northeast-1.amazonaws.com/de
 const submitButton = document.getElementById('submit')
 submitButton.addEventListener('click', async () => {
     const textMessage = document.getElementById('message')
-    console.log("textMessage:" + textMessage.value)
-
-    // AjaxでAPIGWへGET
     const response = await fetch(apigwUrl, {
         method: 'POST',
         headers: {
@@ -13,6 +10,5 @@ submitButton.addEventListener('click', async () => {
         body: JSON.stringify({input_text: textMessage.value})
     })
     const json = await response.json()
-    console.log(json['output_text']);
     document.getElementById('translated-message').value = json['output_text']
 })
